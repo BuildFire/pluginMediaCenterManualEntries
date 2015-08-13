7977a7388a9e8bece4ec7d9891e192feff269b52
deepakshrma <deepak.m.shrma@gmail.com>
2015-08-13 16:48:54 +0530

    updated test cases

--- widget/controllers/widget.media.controller.js
+++ widget/controllers/widget.media.controller.js
@@ -1,1 +1,12 @@
-//dummy file
+(function (angular, window) {
+    angular
+        .module('mediaCenterWidget')
+        .controller('WidgetMediaCtrl', ['$scope', '$window', function ($scope, $window) {
+            var WidgetHome = this;
+            WidgetHome.data = {
+                design: {
+                    listLayout: 'list-1'
+                }
+            }
+        }]);
+})(window.angular, window);
