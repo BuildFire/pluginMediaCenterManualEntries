1f41ef00403ac6326d42a72a81abb60f10bd2534
vineetasharma <vineetasharma18791@gmail.com>
2015-08-14 18:02:24 +0530

    Refactored the code

--- control/content/enums.js
+++ control/content/enums.js
@@ -1,11 +1,17 @@
-'use strict';
-
 (function (angular) {
-    //created mediaCenterContent module
     angular
         .module('mediaCenterContent')
-        .constant('TAG_NAMES', {
-            PEOPLE_INFO: 'mediaInfo',
-            PEOPLE: 'media'
+        .constant('CODES', {
+            NOT_FOUND: 'NOTFOUND',
+            SUCCESS: 'SUCCESS'
         })
-})(window.angular);
+        .constant('MESSAGES', {
+            ERROR: {
+                NOT_FOND: "No result found",
+                CALLBACK_NOT_DEFINED: "Callback is not defined",
+                ID_NOT_DEFINED: "Id is not defined",
+                DATA_NOT_DEFINED: "Data is not defined",
+                OPTION_REQUIRES: "Requires options"
+            }
+        });
+})(window.angular, undefined);
--- control/content/services.js
+++ control/content/services.js
@@ -9,20 +9,124 @@
                 return buildfire;
             }
         }])
-        .factory('Media',[function(){
-            var media=this;
-            media.getById=function(itemId,callback){
-                buildfire.datastore.getById(itemId,'media',callback);
-            };
-            media.add=function(item,callback){
-                buildfire.datastore.insert(item,'media',true,callback);
-            };
-            media.delete=function(itemId,callback){
-                buildfire.datastore.delete(itemId,'media',callback);
-            };
-            media.search=function(options,callback){
-                buildfire.datastore.search(options,'media',callback);
-            };
-            return media;
-        }])
+        .factory("MediaContent", ['Buildfire', '$q', 'MESSAGES', 'CODES', function (Buildfire, $q, MESSAGES, CODES) {
+            var _tagName = "MediaContent";
+            return {
+                get: function () {
+                    var deferred = $q.defer();
+                    Buildfire.datastore.get(_tagName, function (err, result) {
+                        if (err) {
+                            return deferred.reject(err);
+                        }
+                        else if (result && result.data) {
+                            return deferred.resolve(result);
+                        } else {
+                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOND));
+                        }
+                    });
+                    return deferred.promise;
+                },
+                insert: function (items) {
+                    var deferred = $q.defer();
+                    if (typeof items == 'undefined') {
+                        return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
+                    }
+                    if (Array.isArray(items)) {
+                        Buildfire.datastore.bulkInsert(items, _tagName, function (err, result) {
+                            if (err) {
+                                return deferred.reject(err);
+                            }
+                            else if (result) {
+                                return deferred.resolve(result);
+                            } else {
+                                return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOND));
+                            }
+                        });
+                    } else {
+                        Buildfire.datastore.insert(items, _tagName, false, function (err, result) {
+                            if (err) {
+                                return deferred.reject(err);
+                            }
+                            else if (result) {
+                                return deferred.resolve(result);
+                            } else {
+                                return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOND));
+                            }
+                        });
+                    }
+                    return deferred.promise;
+                },
+                find: function (options) {
+                    var deferred = $q.defer();
+                    if (typeof options == 'undefined') {
+                        return deferred.reject(new Error(MESSAGES.ERROR.OPTION_REQUIRES));
+                    }
+                    Buildfire.datastore.search(options, _tagName, function (err, result) {
+                        if (err) {
+                            return deferred.reject(err);
+                        }
+                        else if (result) {
+                            return deferred.resolve(result);
+                        } else {
+                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOND));
+                        }
+                    });
+                    return deferred.promise;
+                },
+                update: function (id, item) {
+                    var deferred = $q.defer();
+                    if (typeof id == 'undefined') {
+                        return deferred.reject(new Error(MESSAGES.ERROR.ID_NOT_DEFINED));
+                    }
+                    if (typeof item == 'undefined') {
+                        return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
+                    }
+                    Buildfire.datastore.update(id, item, _tagName, function (err, result) {
+                        if (err) {
+                            return deferred.reject(err);
+                        }
+                        else if (result) {
+                            return deferred.resolve(result);
+                        } else {
+                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOND));
+                        }
+                    })
+                    return deferred.promise;
+                },
+                save: function (item) {
+                    var deferred = $q.defer();
+                    if (typeof item == 'undefined') {
+                        return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
+                    }
+                    Buildfire.datastore.save(item, _tagName, function (err, result) {
+                        if (err) {
+                            return deferred.reject(err);
+                        }
+                        else if (result) {
+                            return deferred.resolve(result);
+                        } else {
+                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOND));
+                        }
+                    });
+                    return deferred.promise;
+                },
+                delete: function (id) {
+                    var deferred = $q.defer();
+                    if (typeof id == 'undefined') {
+                        return deferred.reject(new Error(MESSAGES.ERROR.ID_NOT_DEFINED));
+                    }
+                    Buildfire.datastore.delete(id, _tagName, function (err, result) {
+                        if (err) {
+                            return deferred.reject(err);
+                        }
+                        else if (result) {
+                            return deferred.resolve(result);
+                        } else {
+                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOND));
+                        }
+                    });
+                    return deferred.promise;
+                }
+            }
+        }]);
 })(window.angular, window.buildfire);
