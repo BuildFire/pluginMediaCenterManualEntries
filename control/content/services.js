103c50826d2b647f3245eca24b624f7768017e03
vineetasharma <vineetasharma18791@gmail.com>
2015-08-14 16:06:57 +0530

    Media service added

--- control/content/services.js
+++ control/content/services.js
@@ -9,4 +9,20 @@
                 return buildfire;
             }
         }])
+        .factory('Media',[function(){
+            var media=this;
+            media.getById=function(itemId,callback){
+                buildfire.datastore.getById(itemId,'media',callback);
+            };
+            media.add=function(item,callback){
+                buildfire.datastore.insert(item,'media',true,callback);
+            };
+            media.delete=function(itemId,callback){
+                buildfire.datastore.delete(itemId,'media',callback);
+            };
+            media.search=function(options,callback){
+                buildfire.datastore.search(options,'media',callback);
+            };
+            return media;
+        }])
 })(window.angular, window.buildfire);
