8d3a973895f9fda9749ed66f41b0777ad9fd3912
vineetasharma <vineetasharma18791@gmail.com>
2015-08-14 13:11:32 +0530

    Write test cases for all routes of Content module

--- test/control/app.js
+++ test/control/app.js
@@ -1,7 +1,6 @@
 describe('mediaCenterContent', function () {
-
-    var location, route, rootScope;
     beforeEach(module('mediaCenterContent'));
+    var location, route, rootScope;
     beforeEach(inject(
         function( _$location_, _$route_, _$rootScope_ ) {
             location = _$location_;
@@ -12,15 +11,47 @@ describe('mediaCenterContent', function () {
     describe('Home route', function() {
         beforeEach(inject(
             function($httpBackend) {
-                $httpBackend.expectGET('')
+                $httpBackend.expectGET('templates/home.html')
+                    .respond(200);
+                $httpBackend.expectGET('/')
                     .respond(200);
             }));
 
-        /*it('should load the login page on successful load of /', function() {
+        it('should load the home page on successful load of /', function() {
             location.path('/');
             rootScope.$digest();
-            expect(route.current.current.controller).toBe('ContentHomeCtrl')
-        });*/
+            expect(route.current.controller).toBe('ContentHomeCtrl')
+        });
     });
 
+    describe('Media route', function() {
+        beforeEach(inject(
+            function($httpBackend) {
+                $httpBackend.expectGET('templates/media.html')
+                    .respond(200);
+                $httpBackend.expectGET('/media')
+                    .respond(200);
+            }));
+
+        it('should load the media page on successful load of /media', function() {
+            location.path('/media');
+            rootScope.$digest();
+            expect(route.current.controller).toBe('ContentMediaCtrl')
+        });
+    });
+    describe('Media Edit route', function() {
+        beforeEach(inject(
+            function($httpBackend) {
+                $httpBackend.expectGET('templates/media.html')
+                    .respond(200);
+                $httpBackend.expectGET('/media/:itemId')
+                    .respond(200);
+            }));
+
+        it('should load the media Edit page on successful load of /media', function() {
+            location.path('/media/:itemId');
+            rootScope.$digest();
+            expect(route.current.controller).toBe('ContentMediaCtrl')
+        });
+    });
 });
