675cd6536816cd085452115b5415a0b6b5918e8d
vineetasharma <vineetasharma18791@gmail.com>
2015-08-14 16:10:04 +0530

    insert new item functionality implemented

--- control/content/controllers/content.media.controller.js
+++ control/content/controllers/content.media.controller.js
@@ -1,7 +1,7 @@
 (function (angular) {
     angular
         .module('mediaCenterContent')
-        .controller('ContentMediaCtrl', ['$scope', '$window','TAG_NAMES','Buildfire', function ($scope, $window,TAG_NAMES,Buildfire) {
+        .controller('ContentMediaCtrl', ['$scope', '$window','TAG_NAMES','Buildfire','Media','$routeParams',function ($scope, $window,TAG_NAMES,Buildfire,Media,$routeParams) {
             var ContentMedia = this;
             ContentMedia.data = {};
 
@@ -25,5 +25,62 @@
                 ContentMedia.data.topImage = null;
             };
 
+            if($routeParams.id){
+                Media.getById($routeParams.id,function(err,result){
+                    if(err){
+                        console.error('Error while getting data------',err);
+                    }
+                    else{
+                        ContentMedia.data=result;
+                    }
+                });
+            }
+
+            ContentMedia.done=function(){
+                Media.add(ContentMedia.data,function(err,result){
+                    if(err){
+                        console.error('-----Error while adding media-----',err);
+                    }
+                    else{
+                        console.log('Results-------',result);
+                    }
+
+                });
+            };
+            ContentMedia.delete=function(){
+                Media.delete(ContentMedia.data.id,function(err,result){
+                    if(err){
+                        console.error('-----Error while adding media-----',err);
+                    }
+                    else{
+                        console.log('Results-------',result);
+                    }
+
+                });
+            };
+
+           /* var tmrDelayForPeoples = null;
+            var updateItemsWithDelay = function (item) {
+                if (tmrDelayForPeoples) {
+                    clearTimeout(tmrDelayForPeoples);
+                    ContentMedia.isUpdating = false;
+                }
+                ContentMedia.unchangedData = angular.equals(_data, ContentMedia.data);
+                ContentMedia.isItemValid = isValidItem(ContentMedia.data);
+                if (!ContentMedia.isUpdating && !isUnchanged(ContentMedia.item) && ContentMedia.isItemValid) {
+                    tmrDelayForPeoples = setTimeout(function () {
+                        if (item.id) {
+                            ContentMedia.updateItemData();
+                        }
+                        else if (!ContentMedia.isNewItemInserted) {
+                            ContentMedia.addNewItem();
+                        }
+                    }, 1000);
+                }
+            };
+
+            $scope.$watch(function () {
+                return ContentMedia.data;
+            }, updateItemsWithDelay, true);*/
         }]);
 })(window.angular);
