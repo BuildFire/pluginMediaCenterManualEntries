2820f6c1870b4aefbe70b4c8d8edcf587ef5f476
vineetasharma <vineetasharma18791@gmail.com>
2015-08-14 15:09:28 +0530

    media topImage added in content

--- control/content/controllers/content.media.controller.js
+++ control/content/controllers/content.media.controller.js
@@ -3,10 +3,27 @@
         .module('mediaCenterContent')
         .controller('ContentMediaCtrl', ['$scope', '$window','TAG_NAMES','Buildfire', function ($scope, $window,TAG_NAMES,Buildfire) {
             var ContentMedia = this;
-            ContentMedia.data = {
-                design: {
-                    listLayout: 'list-1'
+            ContentMedia.data = {};
+
+            var options = {showIcons: false, multiSelection: false};
+            var callback = function (error, result) {
+                if (error) {
+                    return console.error('Error:', error);
                 }
-            }
+                if (result.selectedFiles && result.selectedFiles.length) {
+                    console.log(result.selectedFiles);
+                    ContentMedia.data.topImage = result.selectedFiles[0];
+                    $scope.$digest();
+                }
+            };
+
+            ContentMedia.selectTopImage = function () {
+                Buildfire.imageLib.showDialog(options, callback);
+            };
+
+            ContentMedia.removeTopImage = function () {
+                ContentMedia.data.topImage = null;
+            };
+
         }]);
 })(window.angular);
