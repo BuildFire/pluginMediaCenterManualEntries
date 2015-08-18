04c3b7263da32fe67018c26324ba707abb53a252
lakshaydulani <lakshaydulani@gmail.com>
2015-08-18 15:38:25 +0530

    BF-012: Bootstrapping of Design View

---
+++ control/design/services.js
@@ -0,0 +1,180 @@
+'use strict';
+
+(function (angular, buildfire, location) {
+    //created mediaCenterWidget module
+    angular
+        .module('mediaCenterServices', ['mediaCenterEnums'])
+        .provider('Buildfire', [function () {
+            this.$get = function () {
+                return buildfire;
+            }
+        }])
+        .factory('Location', [function () {
+            var _location = location;
+            return {
+                go: function (path) {
+                    _location.href = path;
+                },
+                goToHome: function () {
+                    _location.href = _location.href.substr(0, _location.href.indexOf('#'));
+                }
+            };
+        }])
+        .factory('Orders', [function () {
+            var ordersMap = {
+                Manually: "Manually",
+                Newest: " Newest",
+                Oldest: " Oldest",
+                Most: " Oldest",
+                Least: " Oldest"
+            }
+            var orders = [
+                {id: 1, name: "Manually", value: "Manually"},
+                {id: 1, name: "Newest", value: "Newest"},
+                {id: 1, name: "Oldest", value: "Oldest"},
+                {id: 1, name: "Most", value: "Most Items"},
+                {id: 1, name: "Least", value: "Least Items"}
+            ];
+            return {
+                ordersMap: ordersMap,
+                getOrder: function (name) {
+                    return orders.filter(function (order) {
+                        return order.name === name;
+                    })[0];
+                }
+            }
+        }])
+        .factory("DB", ['Buildfire', '$q', 'MESSAGES', 'CODES', function (Buildfire, $q, MESSAGES, CODES) {
+            function DB(tagName) {
+                this._tagName = tagName;
+            }
+
+            DB.prototype.get = function () {
+                var that = this;
+                var deferred = $q.defer();
+                Buildfire.datastore.get(that._tagName, function (err, result) {
+                    if (err && err.code == CODES.NOT_FOUND) {
+                        return deferred.resolve();
+                    }
+                    else if (err) {
+                        return deferred.reject(err);
+                    }
+                    else {
+                        return deferred.resolve(result);
+                    }
+                });
+                return deferred.promise;
+            }
+            DB.prototype.insert = function (items) {
+                var that = this;
+                var deferred = $q.defer();
+                if (typeof items == 'undefined') {
+                    return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
+                }
+                if (Array.isArray(items)) {
+                    Buildfire.datastore.bulkInsert(items, that._tagName, function (err, result) {
+                        if (err) {
+                            return deferred.reject(err);
+                        }
+                        else if (result) {
+                            return deferred.resolve(result);
+                        } else {
+                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
+                        }
+                    });
+                } else {
+                    Buildfire.datastore.insert(items, that._tagName, false, function (err, result) {
+                        if (err) {
+                            return deferred.reject(err);
+                        }
+                        else if (result) {
+                            return deferred.resolve(result);
+                        } else {
+                            return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
+                        }
+                    });
+                }
+                return deferred.promise;
+            };
+            DB.prototype.find = function (options) {
+                var that = this;
+                var deferred = $q.defer();
+                if (typeof options == 'undefined') {
+                    return deferred.reject(new Error(MESSAGES.ERROR.OPTION_REQUIRES));
+                }
+                Buildfire.datastore.search(options, that._tagName, function (err, result) {
+                    if (err) {
+                        return deferred.reject(err);
+                    }
+                    else if (result) {
+                        return deferred.resolve(result);
+                    } else {
+                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
+                    }
+                });
+                return deferred.promise;
+            }
+            DB.prototype.update = function (id, item) {
+                var that = this;
+                var deferred = $q.defer();
+                if (typeof id == 'undefined') {
+                    return deferred.reject(new Error(MESSAGES.ERROR.ID_NOT_DEFINED));
+                }
+                if (typeof item == 'undefined') {
+                    return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
+                }
+                Buildfire.datastore.update(id, item, that._tagName, function (err, result) {
+                    if (err) {
+                        return deferred.reject(err);
+                    }
+                    else if (result) {
+                        return deferred.resolve(result);
+                    } else {
+                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
+                    }
+                })
+                return deferred.promise;
+            };
+            DB.prototype.save = function (item) {
+                var that = this;
+                var deferred = $q.defer();
+                if (typeof item == 'undefined') {
+                    return deferred.reject(new Error(MESSAGES.ERROR.DATA_NOT_DEFINED));
+                }
+                Buildfire.datastore.save(item, that._tagName, function (err, result) {
+                    if (err) {
+                        return deferred.reject(err);
+                    }
+                    else if (result) {
+                        return deferred.resolve(result);
+                    } else {
+                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
+                    }
+                });
+                return deferred.promise;
+            };
+            DB.prototype.delete = function (id) {
+                var that = this;
+                var deferred = $q.defer();
+                if (typeof id == 'undefined') {
+                    return deferred.reject(new Error(MESSAGES.ERROR.ID_NOT_DEFINED));
+                }
+                Buildfire.datastore.delete(id, that._tagName, function (err, result) {
+                    if (err) {
+                        return deferred.reject(err);
+                    }
+                    else if (result) {
+                        return deferred.resolve(result);
+                    } else {
+                        return deferred.reject(new Error(MESSAGES.ERROR.NOT_FOUND));
+                    }
+                });
+                return deferred.promise;
+            }
+            return DB;
+        }])
+        .factory('Utility', [function () {
+
+        }])
+
+})(window.angular, window.buildfire, window.location);
