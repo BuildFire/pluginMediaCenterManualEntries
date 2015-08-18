3d0d376de9931c2e292ca13e074da9d8dfd86ed1
vineetasharma <vineetasharma18791@gmail.com>
2015-08-18 12:05:46 +0530

    BF-015: Code refactored

--- control/content/controllers/content.media.controller.js
+++ control/content/controllers/content.media.controller.js
@@ -1,127 +1,42 @@
 (function (angular) {
     angular
         .module('mediaCenterContent')
-        .controller('ContentMediaCtrl', ['$scope', '$window','Buildfire','DB', 'COLLECTIONS','$routeParams','Location','media',function ($scope, $window,Buildfire,DB,COLLECTIONS,$routeParams,Location,media) {
+        .controller('ContentMediaCtrl', ['$scope', '$window', 'Buildfire', 'DB', 'COLLECTIONS', 'Location', 'media', function ($scope, $window, Buildfire, DB, COLLECTIONS, Location, media) {
             var ContentMedia = this;
-            ContentMedia.isNewItemInserted = false;
-            ContentMedia.unchangedData = true;
-
-            ContentMedia.linksSortableOptions = {
-                handle: '> .handle'
-            };
-            ContentMedia.bodyContentWYSIWYGOptions={
-                plugins: 'advlist autolink link image lists charmap print preview',
-                skin: 'lightgray',
-                trusted: true,
-                theme: 'modern'
-            };
-            var data={
-                topImage: '',
-                summary: '',
-                body: '',
-                srcUrl: '',
-                audioUrl: '',
-                videoUrl: '',
-                image: '',
-                dateCreated:+new Date(),
-                rank: '',
-                links: [] // this will contain action links
-            };
-            if(media){
-              ContentMedia.item=media;
-            }
-            else {
-                ContentMedia.item={data:data};
-            }
+            var tmrDelayForMedia = null;
             var MediaContent = new DB(COLLECTIONS.MediaContent);
-            var options = {showIcons: false, multiSelection: false};
-            var callback = function (error, result) {
-                if (error) {
-                    return console.error('Error:', error);
-                }
-                if (result.selectedFiles && result.selectedFiles.length) {
-                    ContentMedia.item.data.topImage = result.selectedFiles[0];
-                    $scope.$digest();
-                }
-            };
 
-            ContentMedia.selectTopImage = function () {
-                Buildfire.imageLib.showDialog(options, callback);
-            };
-
-            ContentMedia.removeTopImage = function () {
-                ContentMedia.item.data.topImage = null;
-            };
-            ContentMedia.selectAudioImage = function () {
-                Buildfire.imageLib.showDialog(options,function (error, result) {
-                    if (error) {
-                        return console.error('Error:', error);
-                    }
-                    if (result.selectedFiles && result.selectedFiles.length) {
-                        ContentMedia.item.data.image = result.selectedFiles[0];
-                        $scope.$digest();
-                    }
-                });
-            };
-
-            ContentMedia.removeAudioImage = function () {
-                ContentMedia.item.data.image = null;
-            };
-
-            ContentMedia.openAddLinkPopup = function () {
-                var options = {showIcons: false};
-                var callback = function (error, result) {
-                    console.log(result);
-                    if (error) {
-                        return console.error('Error:', error);
-                    }
-                    if (!ContentMedia.item.data.links)
-                        ContentMedia.item.data.links = [];
-                    ContentMedia.item.data.links.push(result);
-                    $scope.$digest();
+            function init() {
+                var data = {
+                    topImage: '',
+                    summary: '',
+                    body: '',
+                    srcUrl: '',
+                    audioUrl: '',
+                    videoUrl: '',
+                    image: '',
+                    dateCreated: '',
+                    rank: '',
+                    links: [] // this will contain action links
                 };
-                Buildfire.actionItems.showDialog(null, options, callback);
-            };
-
-            ContentMedia.openEditLinkPopup = function (link,index) {
-                var options = {showIcons: false};
-                var callback = function (error, result) {
-                    console.log(result);
-                    if (error) {
-                        return console.error('Error:', error);
-                    }
-                    if (!ContentMedia.item.data.links)
-                        ContentMedia.item.data.links = [];
-                    ContentMedia.item.data.links.splice(index,1,result);
-                    $scope.$digest();
+                ContentMedia.linksSortableOptions = {
+                    handle: '> .handle'
                 };
-                Buildfire.actionItems.showDialog(link, options, callback);
-            };
-
-            ContentMedia.removeLink=function(index){
-                if(ContentMedia.item && ContentMedia.item.data && ContentMedia.item.data.links)
-                ContentMedia.item.data.links.splice(index,1);
-            };
-
-            ContentMedia.done=function(){
-                if(ContentMedia.item && ContentMedia.item.id) {
-                    MediaContent.update(ContentMedia.item.id,ContentMedia.item.data);
+                ContentMedia.bodyContentWYSIWYGOptions = {
+                    plugins: 'advlist autolink link image lists charmap print preview',
+                    skin: 'lightgray',
+                    trusted: true,
+                    theme: 'modern'
+                };
+                if (media) {
+                    ContentMedia.item = media;
                 }
+                else {
+                    ContentMedia.item = {data: data};
+                }
+                updateMasterItem(ContentMedia.item);
+            }
 
-                Location.goToHome();
-
-            };
-
-            ContentMedia.delete=function(){
-                if(ContentMedia.item && ContentMedia.item.id)
-                MediaContent.delete(ContentMedia.item.id).then(function(data){
-                    Location.goToHome();
-                },function(err){
-                    console.error('Error while deleting an item-----',err);
-                });
-            };
-
-            updateMasterItem(ContentMedia.item);
             function updateMasterItem(item) {
                 ContentMedia.masterItem = angular.copy(item);
             }
@@ -134,48 +49,138 @@
                 return angular.equals(item, ContentMedia.masterItem);
             }
 
-             function updateItemData() {
-                 MediaContent.update(ContentMedia.item.id,ContentMedia.item.data).then(function(data){
-                     updateMasterItem(ContentMedia.item);
-                 },function(err){
-                     console.error('Error-------',err);
-                 });
-            }
-
-            function addNewItem(){
-                MediaContent.insert(ContentMedia.item.data).then(function(data){
-                    MediaContent.getById(data.id).then(function(data){
-                        ContentMedia.item=data;
-                        updateMasterItem(data);
-                    },function(err){
-                        ContentMedia.item=ContentMedia.masterItem;
-                        console.error('Error while getting----------',err);
-                    });
-                },function(err){
-                    console.error('---------------Error while inserting data------------',err);
+            function updateItemData() {
+                MediaContent.update(ContentMedia.item.id, ContentMedia.item.data).then(function (data) {
+                    updateMasterItem(ContentMedia.item);
+                }, function (err) {
+                    resetItem();
+                    console.error('Error-------', err);
                 });
             }
 
-            var tmrDelayForMedia = null;
-            var updateItemsWithDelay = function (item) {
+            function addNewItem() {
+                MediaContent.insert(ContentMedia.item.data).then(function (data) {
+                    MediaContent.getById(data.id).then(function (data) {
+                        ContentMedia.item = data;
+                        updateMasterItem(data);
+                    }, function (err) {
+                        resetItem();
+                        console.error('Error while getting----------', err);
+                    });
+                }, function (err) {
+                    console.error('---------------Error while inserting data------------', err);
+                });
+            }
+
+            function updateItemsWithDelay(item) {
                 if (tmrDelayForMedia) {
                     clearTimeout(tmrDelayForMedia);
                 }
-                ContentMedia.unchangedData = angular.equals(ContentMedia.masterItem, ContentMedia.item.data);
                 if (!isUnchanged(ContentMedia.item)) {
                     tmrDelayForMedia = setTimeout(function () {
                         if (item.id) {
                             updateItemData();
                         }
-                        else if (!ContentMedia.isNewItemInserted) {
+                        else {
+                            ContentMedia.item.data.dateCreated=+new Date();
                             addNewItem();
                         }
                     }, 1000);
                 }
+            }
+
+            var selectImageOptions = {showIcons: false, multiSelection: false};
+            var topImagecallback = function (error, result) {
+                if (error) {
+                    return console.error('Error:', error);
+                }
+                if (result.selectedFiles && result.selectedFiles.length) {
+                    ContentMedia.item.data.topImage = result.selectedFiles[0];
+                    $scope.$digest();
+                }
+            };
+
+            ContentMedia.selectTopImage = function () {
+                Buildfire.imageLib.showDialog(selectImageOptions, topImagecallback);
+            };
+
+            ContentMedia.removeTopImage = function () {
+                ContentMedia.item.data.topImage = '';
+            };
+            var audioImageCallback = function (error, result) {
+                if (error) {
+                    return console.error('Error:', error);
+                }
+                if (result.selectedFiles && result.selectedFiles.length) {
+                    ContentMedia.item.data.image = result.selectedFiles[0];
+                    $scope.$digest();
+                }
+            };
+            ContentMedia.selectAudioImage = function () {
+                Buildfire.imageLib.showDialog(selectImageOptions, audioImageCallback);
+            };
+
+            ContentMedia.removeAudioImage = function () {
+                ContentMedia.item.data.image = '';
+            };
+
+            var linksOptions = {showIcons: false};
+            var addLinkCallback = function (error, result) {
+                if (error) {
+                    return console.error('Error:', error);
+                }
+                if (!ContentMedia.item.data.links)
+                    ContentMedia.item.data.links = [];
+                ContentMedia.item.data.links.push(result);
+                $scope.$digest();
+            };
+
+            ContentMedia.openAddLinkPopup = function () {
+                Buildfire.actionItems.showDialog(null, linksOptions, addLinkCallback);
+            };
+
+            ContentMedia.openEditLinkPopup = function (link, index) {
+                Buildfire.actionItems.showDialog(link, linksOptions, function (error, result) {
+                    if (error) {
+                        return console.error('Error:', error);
+                    }
+                    if (!ContentMedia.item.data.links)
+                        ContentMedia.item.data.links = [];
+                    ContentMedia.item.data.links.splice(index, 1, result);
+                    $scope.$digest();
+                });
+            };
+
+            ContentMedia.removeLink = function (index) {
+                if (ContentMedia.item.data && ContentMedia.item.data.links)
+                    ContentMedia.item.data.links.splice(index, 1);
+            };
+
+            ContentMedia.done = function () {
+                if (ContentMedia.item.id) {
+                    MediaContent.update(ContentMedia.item.id, ContentMedia.item.data).then(function (data) {
+                            Location.goToHome();
+                    }, function (err) {
+                        console.error('error----',err);
+                        //do something on error
+                    });
+                }
+
+            };
+
+            ContentMedia.delete = function () {
+                if ( ContentMedia.item.id)
+                    MediaContent.delete(ContentMedia.item.id).then(function (data) {
+                        Location.goToHome();
+                    }, function (err) {
+                        console.error('Error while deleting an item-----', err);
+                    });
             };
 
             $scope.$watch(function () {
                 return ContentMedia.item;
             }, updateItemsWithDelay, true);
+
+            init();
         }]);
 })(window.angular);
