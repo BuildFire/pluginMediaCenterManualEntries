9016d98c984bd41ed18787bcc5dd356bd8327290
deepakshrma <deepak.m.shrma@gmail.com>
2015-08-18 18:01:26 +0530

    BF-005: Remove image on delete

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -6,17 +6,34 @@
         .controller('ContentHomeCtrl', ['$scope', 'MediaCenterInfo', 'Modals', function ($scope, MediaCenterInfo, Modals) {
             var ContentHome = this;
             ContentHome.info = MediaCenterInfo;
-            ContentHome.bodyWYSIWYGOptions={
+
+            //option for wysiwyg
+            ContentHome.bodyWYSIWYGOptions = {
                 plugins: 'advlist autolink link image lists charmap print preview',
                 skin: 'lightgray',
                 trusted: true,
                 theme: 'modern'
             };
-
-            ContentHome.rmCarouselImage = function () {
-
+            //on remove button click remove carousel Image
+            ContentHome.rmCarouselImage = function (index) {
+                if (typeof index == "undefined") {
+                    return;
+                }
+                var image = ContentHome.info.data.content.images[index]
+                if (typeof image == "undefined") {
+                    return;
+                }
+                Modals.removePopupModal({title: image.title}).then(function (result) {
+                    if (result)
+                        ContentHome.info.data.content.images.splice(index, 1);
+                    else {
+                        console.info('Unable to load data.')
+                    }
+                }, function (cancelData) {
+                    //do something on cancel
+                });
             }
-            ContentHome.addCarouselImage = function (index) {
+            ContentHome.addUpdateCarouselImage = function (index) {
                 var link = null;
                 if (typeof index != 'undefined') {
                     link = ContentHome.info.data.content.images[index];
--- control/content/modals.js
+++ control/content/modals.js
@@ -28,6 +28,28 @@
                         carDeferred.reject(err);
                     });
                     return carDeferred.promise;
+                },
+                removePopupModal: function (info) {
+                    var removePopupDeferred = $q.defer();
+                    var removePopupModal = $modal
+                        .open({
+                            templateUrl: 'templates/modals/rm-image-link-modal.html',
+                            controller: 'RemovePopupCtrl',
+                            controllerAs: 'RemovePopup',
+                            size: 'sm',
+                            resolve: {
+                                Info: function () {
+                                    return info;
+                                }
+                            }
+                        });
+                    removePopupModal.result.then(function (imageInfo) {
+                        removePopupDeferred.resolve(imageInfo);
+                    }, function (err) {
+                        //do something on cancel
+                        removePopupDeferred.reject(err);
+                    });
+                    return removePopupDeferred.promise;
                 }
             };
         }])
@@ -75,5 +97,18 @@
                 CarouselImage.link.imageUrl = '';
             };
 
+        }])
+        .controller('RemovePopupCtrl', ['$scope', '$modalInstance', 'Info', function ($scope, $modalInstance, Info) {
+            var RemovePopup = this;
+            RemovePopup.info = {};
+            if (Info) {
+                RemovePopup.info = Info;
+            }
+            RemovePopup.ok = function () {
+                $modalInstance.close('yes');
+            };
+            RemovePopup.cancel = function () {
+                $modalInstance.dismiss('no');
+            };
         }])
 })(window.angular, window.buildfire);
