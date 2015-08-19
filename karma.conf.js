b62aa42ff7bdf386198be9c47d5eb3b01c857234
deepakshrma <deepak.m.shrma@gmail.com>
2015-08-19 16:02:22 +0530

    BF-004: Coverage plugin install

--- karma.conf.js
+++ karma.conf.js
@@ -38,17 +38,25 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'widget/**/!(js)/*.js': ['coverage'],
+      'widget/*.js': ['coverage']
     },
 
     plugins:[
       'karma-phantomjs-launcher',
       'karma-jasmine',
-      'karma-junit-reporter'],
+      'karma-junit-reporter',
+      'karma-coverage'
+    ],
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
 
     // web server port
     port: 9876,
@@ -74,6 +82,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   })
 }
