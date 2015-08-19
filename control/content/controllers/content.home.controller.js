76d8810880c51269d2e9f56e258153e7081bef6c
vineetasharma <vineetasharma18791@gmail.com>
2015-08-19 11:32:00 +0530

    Resolved conflicts

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -3,24 +3,64 @@
 (function (angular, window) {
     angular
         .module('mediaCenterContent')
-        .controller('ContentHomeCtrl', ['$scope', 'MediaCenterInfo', 'Modals', 'DB', 'COLLECTIONS','Orders', function ($scope, MediaCenterInfo, Modals, DB, COLLECTIONS,Orders) {
-            var MediaCenter = new DB(COLLECTIONS.MediaCenter);
+        .controller('ContentHomeCtrl', ['$scope', 'MediaCenterInfo', 'Modals', 'DB', 'COLLECTIONS', 'Orders', function ($scope, MediaCenterInfo, Modals, DB, COLLECTIONS, Orders) {
             var ContentHome = this;
-            ContentHome.orders=Orders.ordersMap;
             ContentHome.info = MediaCenterInfo;
             updateMasterInfo(ContentHome.info);
             var tmrDelayForMedia=null;
+            var MediaCenter = new DB(COLLECTIONS.MediaContent);
+            var _page = 0,
+                _pageSize = 10,
+                searchOptions = {
+                    filter: {"$json.title": {"$regex": '/*'}},
+                    page: _page,
+                    pageSize: _pageSize + 1 // the plus one is to check if there are any more
+                };
+            //option for wysiwyg
             ContentHome.bodyWYSIWYGOptions = {
                 plugins: 'advlist autolink link image lists charmap print preview',
                 skin: 'lightgray',
                 trusted: true,
                 theme: 'modern'
             };
-
-            ContentHome.rmCarouselImage = function () {
-
+            ContentHome.isBusy = false;
+            ContentHome.hasMore = false;
+            ContentHome.items = [];
+            ContentHome.sortOptions = Orders.options;
+            //on remove button click remove carousel Image
+            ContentHome.rmCarouselImage = function (index) {
+                if (typeof index == "undefined") {
+                    return;
+                }
+                var image = ContentHome.info.data.content.images[index]
+                if (typeof image == "undefined") {
+                    return;
+                }
+                Modals.removePopupModal({title: image.title}).then(function (result) {
+                    if (result)
+                        ContentHome.info.data.content.images.splice(index, 1);
+                    else {
+                        console.info('Unable to load data.')
+                    }
+                }, function (cancelData) {
+                    //do something on cancel
+                });
             }
-            ContentHome.addCarouselImage = function (index) {
+
+            var updateSearchOptions = function () {
+                var order = Orders.getOrder(ContentHome.info.data.content.sortBy || Orders.ordersMap.Default);
+                if (order) {
+                    var sort = {};
+                    sort[order.key] = order.order;
+                    searchOptions.sort = sort;
+                    return searchOptions;
+                    ContentHome.itemSortableOptions.disabled == !(ContentHome.info.data.content.sortBy === Orders.ordersMap.Manually)
+                }
+                else {
+                    return;
+                }
+            };
+            ContentHome.addUpdateCarouselImage = function (index) {
                 var link = null;
                 if (typeof index != 'undefined') {
                     link = ContentHome.info.data.content.images[index];
@@ -45,6 +85,73 @@
             ContentHome.carouselOptions = {
                 handle: '> .cursor-grab'
             };
+            ContentHome.more = function (search) {
+                if (ContentHome.isBusy) {
+                    return;
+                }
+
+                if (updateSearchOptions()) {
+                    ContentHome.isBusy = true;
+                    MediaCenter.find(searchOptions).then(function success(result) {
+                        if (result.length > _pageSize) {// to indicate there are more
+                            result.pop();
+                            searchOptions.page = searchOptions.page + 1;
+                            ContentHome.hasMore = true;
+                        } else {
+                            ContentHome.hasMore = false;
+                        }
+                        ContentHome.items = ContentHome.items ? ContentHome.items.concat(result) : result;
+                        ContentHome.isBusy = false;
+                    }, function fail() {
+                        ContentHome.isBusy = false;
+                    })
+                }
+            };
+            ContentHome.toggleSortOrder = function (name) {
+               ContentHome.info.data.content.sortBy = name;
+            };
+            ContentHome.itemSortableOptions = {
+                handle: '> .cursor-grab',
+                disabled: !(ContentHome.info.data.content.sortBy === Orders.ordersMap.Manually),
+                stop: function (e, ui) {
+                    var endIndex = ui.item.sortable.dropindex,
+                        maxRank = 0,
+                        draggedItem = ContentHome.items[endIndex];
+
+                    if (draggedItem) {
+                        var prev = ContentHome.items[endIndex - 1],
+                            next = ContentHome.items[endIndex + 1];
+                        var isRankChanged = false;
+                        if (next) {
+                            if (prev) {
+                                draggedItem.data.rank = ((prev.data.rank || 0) + (next.data.rank || 0)) / 2;
+                                isRankChanged = true;
+                            } else {
+                                draggedItem.data.rank = (next.data.rank || 0) / 2;
+                                isRankChanged = true;
+                            }
+                        } else {
+                            if (prev) {
+                                draggedItem.data.rank = (((prev.data.rank || 0) * 2) + 10) / 2;
+                                maxRank = draggedItem.data.rank;
+                                isRankChanged = true;
+                            }
+                        }
+                        if (isRankChanged) {
+                            //Buildfire.datastore.update(draggedItem.id, draggedItem.data, TAG_NAMES.PEOPLE, function (err) {
+                            //    if (err) {
+                            //        console.error('Error during updating rank');
+                            //    } else {
+                            //        if (ContentHome.data.content.rankOfLastItem < maxRank) {
+                            //            ContentHome.data.content.rankOfLastItem = maxRank;
+                            //            RankOfLastItem.setRank(maxRank);
+                            //        }
+                            //    }
+                            //})
+                        }
+                    }
+                }
+            };
             function updateMasterInfo(info) {
                 ContentHome.masterInfo = angular.copy(info);
             }
@@ -67,7 +174,6 @@
             }
 
             function saveDataWithDelay(info) {
-                console.log('Save Data With Delay method called-----------',ContentHome.info);
                 if (tmrDelayForMedia) {
                     clearTimeout(tmrDelayForMedia);
                 }
@@ -81,5 +187,6 @@
             $scope.$watch(function () {
                 return ContentHome.info;
             }, saveDataWithDelay, true);
+
         }])
 })(window.angular, window);
