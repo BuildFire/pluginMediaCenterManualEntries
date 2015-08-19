2532ee64b961c2bba0d391bbdbd4c74b82385ff0
deepakshrma <deepak.m.shrma@gmail.com>
2015-08-19 16:24:09 +0530

    added missing file

---
+++ widget/filters.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function (angular, buildfire, location) {
+    //created mediaCenterWidget module
+    angular
+        .module('mediaCenterFilters', [])
+        .filter('resizeImage', [function () {
+            return function (url, width, height, type) {
+                return buildfire.imageLib.resizeImage(url, {
+                    width: width,
+                    height: height
+                });
+            }
+        }])
+        .filter('cropImage', [function () {
+            return function (url, width, height, type) {
+                return buildfire.imageLib.cropImage(url, {
+                    width: width,
+                    height: height
+                });
+            }
+        }])
+        .filter('safeHtml', ['$sce', function ($sce) {
+            return function (html) {
+                if (html)
+                    return $sce.trustAsHtml(html);
+                else
+                    return "";
+            }
+        }]);
+})(window.angular, window.buildfire, window.location);
