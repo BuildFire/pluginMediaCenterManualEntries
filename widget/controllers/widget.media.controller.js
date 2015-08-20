cba0d73301055fa185320517a53bcb221311d1f3
vineetasharma <vineetasharma18791@gmail.com>
2015-08-20 15:50:13 +0530

    BF-033: On update layout update

--- widget/controllers/widget.media.controller.js
+++ widget/controllers/widget.media.controller.js
@@ -1,12 +1,13 @@
 (function (angular, window) {
     angular
         .module('mediaCenterWidget')
-        .controller('WidgetMediaCtrl', ['$scope', '$window', 'AppConfig', 'Messaging', 'Buildfire', function ($scope, $window, AppConfig, Messaging, Buildfire) {
+        .controller('WidgetMediaCtrl', ['$scope', '$window', 'AppConfig', 'Messaging', 'Buildfire', 'COLLECTIONS', function ($scope, $window, AppConfig, Messaging, Buildfire, COLLECTIONS) {
             console.log(">>>>>>>>>><<<<<<<<<<<<<<<<<<<")
             var WidgetMedia = this;
             WidgetMedia.media = {
                 data: AppConfig.getSettings()
             };
+            var currentItemLayout=WidgetMedia.media.data.itemLayout;
             Messaging.onReceivedMessage(function (event) {
                 if (event) {
                     switch (event.name) {
@@ -31,6 +32,18 @@
                 }
             });
             Buildfire.datastore.onUpdate(function (event) {
-            })
+                switch (event.tag) {
+                    case COLLECTIONS.MediaContent:
+                        if (event.data) {
+                            if (event.data.design && event.data.design.itemLayout!=currentItemLayout) {
+                                currentItemLayout=event.data.design.itemLayout;
+                                WidgetMedia.media.data.design.itemLayout=event.data.design.itemLayout;
+                                AppConfig.setSettings(event);
+                                $scope.$digest();
+                            }
+                        }
+                        break;
+                }
+            });
         }]);
 })(window.angular, window);
