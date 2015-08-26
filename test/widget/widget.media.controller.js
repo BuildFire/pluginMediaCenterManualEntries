00eaf8f2e1a8dc15b2e96aacd79ca2945f69e1bb
vineetasharma <vineetasharma18791@gmail.com>
2015-08-26 13:16:44 +0530

    Test cases for Widget.home.controller

--- test/widget/widget.media.controller.js
+++ test/widget/widget.media.controller.js
@@ -4,33 +4,78 @@
 describe('WidgetMedia Controller', function () {
     beforeEach(module('mediaCenterWidget'));
 
-    var $controller, COLLECTIONS, DB, $timeout, Buildfire, $rootScope, Messaging, AppConfig, media;
+    var $controller, $scope, Orders, COLLECTIONS, DB, $timeout, Buildfire, $rootScope, Messaging, AppConfig, media, WidgetMedia;
 
-    beforeEach(inject(function (_$controller_, _COLLECTIONS_, _DB_, _$timeout_, _Buildfire_, $q, _$rootScope_, _Messaging_, _AppConfig_, _media_) {
+    beforeEach(inject(function (_$controller_, _COLLECTIONS_, _Orders_, _DB_, _$timeout_, _$rootScope_) {
             // The injector unwraps the underscores (_) from around the parameter names when matching
             $controller = _$controller_;
             COLLECTIONS = _COLLECTIONS_;
             DB = _DB_;
             $timeout = _$timeout_;
-            Buildfire = _Buildfire_;
+            //Buildfire = _Buildfire_;
             $rootScope = _$rootScope_;
-            Messaging = _Messaging_;
-            AppConfig = _AppConfig_;
-            media=_media_;
+            $scope = $rootScope.$new();
+            //Messaging = _Messaging_;
+            //AppConfig = _AppConfig_;
+            Orders = _Orders_;
+            WidgetMedia = $controller('WidgetMediaCtrl', {
+                $scope: $scope,
+                item: {
+                    data: {
+                        content: {
+                            images: [],
+                            descriptionHTML: '',
+                            description: '',
+                            sortBy: Orders.ordersMap.Newest,
+                            rankOfLastItem: 0
+                        },
+                        design: {
+                            listLayout: "list-1",
+                            itemLayout: "item-1",
+                            backgroundImage: ""
+                        }
+                    }
+                },
+                media: {
+                    data: {
+                        content: {
+                            images: [],
+                            descriptionHTML: '',
+                            description: '',
+                            sortBy: Orders.ordersMap.Newest,
+                            rankOfLastItem: 0
+                        },
+                        design: {
+                            listLayout: "list-1",
+                            itemLayout: "item-1",
+                            backgroundImage: "imageurl"
+                        }
+                    }
+                }
+            });
+
         }
     ));
-    describe('WidgetMediaCtrl.media', function () {
+
+    describe('WidgetMedia', function () {
         it('shd be initialised properly', function () {
-            var $scope = $rootScope.$new();
-            var controller = $controller('WidgetMediaCtrl', {$scope: $scope, media: {data: AppConfig.getSettings()}});
-            expect(controller.media).toBeDefined();
+            console.log($controller);
+            expect(WidgetMedia).toBeDefined();
         });
     });
-    describe('WidgetMediaCtrl.item', function () {
-        it('shd be initialised properly', function () {
-            var $scope = $rootScope.$new();
-            var controller = $controller('WidgetMediaCtrl', {$scope: $scope, item: media});
-            expect(controller.item).toBeDefined();
-        });
-    });
+    /* describe('WidgetMediaCtrl.item', function () {
+     it('shd be initialised properly', function () {
+     var $scope = $rootScope.$new();
+     var controller = $controller('WidgetMediaCtrl', {$scope: $scope, item: media});
+     expect(controller.item).toBeDefined();
+     });
+     });
+     describe('WidgetMediaCtrl.changeBackgroundTheme', function () {
+     it('shd be initialised properly', function () {
+     var $scope = $rootScope.$new();
+     var controller = $controller('WidgetMediaCtrl', {$scope: $scope, AppConfig: {changeBackgroundTheme:function(image){}}});
+     controller.AppConfig.changeBackgroundTheme('imageUrl');
+     expect(AppConfig.setSettings());
+     });
+     });*/
 });
