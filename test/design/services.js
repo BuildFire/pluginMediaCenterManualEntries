30c99cf3040a29a3ee3f7925250a9c0299b1a618
lakshaydulani <lakshaydulani@gmail.com>
2015-08-31 14:08:56 +0530

    Video player improvements

---
+++ test/design/services.js
@@ -0,0 +1,62 @@
+/**
+ * Created by lakshay on 28/8/15.
+ */
+
+describe('mediaCenterServices: Services', function () {
+    var COLLECTIONS;
+    beforeEach(module('mediaCenterServices'));
+    beforeEach(inject(function ($injector) {
+        COLLECTIONS = $injector.get('COLLECTIONS');
+    }));
+
+    describe('Buildfire service', function () {
+        var Buildfire;
+        beforeEach(inject(
+            function (_Buildfire_) {
+                Buildfire = _Buildfire_;
+            }));
+        it('Buildfire should exists', function () {
+            expect(Buildfire).toBeDefined();
+        });
+    });
+    describe('MediaCenter service', function () {
+        var DB, MediaCenter;
+        beforeEach(inject(
+            function (_DB_) {
+                DB = _DB_;
+                MediaCenter = new DB(COLLECTIONS.MediaCenter);
+            }));
+        it('MediaCenter should exists', function () {
+            expect(MediaCenter).toBeDefined();
+            expect(MediaCenter._tagName).toEqual(COLLECTIONS.MediaCenter);
+        });
+        /* it('MediaCenter methods should exists', function () {
+         expect(MediaCenter.get).toBeDefined();
+         expect(MediaCenter.find).toBeDefined();
+         expect(MediaCenter.save).toBeDefined();
+         expect(MediaCenter.update).toBeDefined();
+         expect(MediaCenter.delete).toBeDefined();
+         expect(MediaCenter.insert).toBeDefined();
+         });*/
+    });
+    describe('MediaContent service', function () {
+        var DB, MediaContent;
+        beforeEach(inject(
+            function (_DB_) {
+                DB = _DB_;
+                MediaContent = new DB(COLLECTIONS.MediaContent);
+            }));
+        it('MediaContent should exists', function () {
+            expect(MediaContent).toBeDefined();
+            expect(MediaContent._tagName).toEqual(COLLECTIONS.MediaContent);
+        });
+        it('MediaCenter methods should exists', function () {
+            expect(MediaContent.get).toBeDefined();
+            expect(MediaContent.find).toBeDefined();
+            expect(MediaContent.save).toBeDefined();
+            expect(MediaContent.update).toBeDefined();
+            expect(MediaContent.delete).toBeDefined();
+            expect(MediaContent.insert).toBeDefined();
+        });
+    });
+});
---
+++ test/widget/app.filters.js
@@ -0,0 +1,22 @@
+/**
+ * Created by lakshay on 27/8/15.
+ */
+describe('Unit: resizeImage filter', function () {
+    beforeEach(module('mediaCenterFilters'));
+    var filter;
+    beforeEach(inject(function (_$filter_) {
+        filter = _$filter_;
+    }));
+
+    it('it should pass if "resizeImage" filter returns resized image url', function () {
+        var result;
+        result = filter('resizeImage')('https://imagelibserver.s3.amazonaws.com/25935164-2add-11e5-9d04-02f7ca55c361/950a50c0-400a-11e5-9af5-3f5e0d725ccb.jpg', 88, 124, 'resize');
+        expect(result).toEqual("http://s7obnu.cloudimage.io/s/resizenp/88x124/https://imagelibserver.s3.amazonaws.com/25935164-2add-11e5-9d04-02f7ca55c361/950a50c0-400a-11e5-9af5-3f5e0d725ccb.jpg");
+    });
+
+    it('it should give a default image even if parameter is blank', function () {
+        var result;
+        result = filter('resizeImage')('', 88, 124, 'resize');
+        expect(result).toEqual("http://s7obnu.cloudimage.io/s/resizenp/88x124/");
+    });
+});
