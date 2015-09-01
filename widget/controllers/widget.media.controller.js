167501a970b22c969b4a008a4694f7a5da691ddf
lakshaydulani <lakshaydulani@gmail.com>
2015-09-01 18:46:28 +0530

    Show video as per aspect ratio, Non repeating background Image in Widget, Youtube/Vimeo Video Embed Support

--- widget/controllers/widget.media.controller.js
+++ widget/controllers/widget.media.controller.js
@@ -3,89 +3,29 @@
         .module('mediaCenterWidget')
         .controller('WidgetMediaCtrl', ['$scope', '$window', 'AppConfig', 'Messaging', 'Buildfire', 'COLLECTIONS', 'media', 'EVENTS', '$timeout', "$sce", function ($scope, $window, AppConfig, Messaging, Buildfire, COLLECTIONS, media, EVENTS, $timeout, $sce) {
             var WidgetMedia = this;
-            $scope.audio=false;
-            WidgetMedia.listen=function(){
-                console.log('method called');
-                $scope.audio=true;
-            };
-            var audioPlayer=Buildfire.services.media.audioPlayer;
-            audioPlayer.onEvent (function(e){
-                if(e.event =="timeUpdate"){
-                    $scope.currentTime = e.data.currentTime;
-                    $scope.duration = e.data.duration;
-                    $scope.$apply();
-                }
-                else if(e.event =="audioEnded" && $scope.currentTrack){
-                    $scope.currentTrack.playing = false;
-                    $scope.$apply();
-                }
-            });
-            WidgetMedia.playAudio=function(){
-                if(WidgetMedia.item.data.audioUrl){
-                    audioPlayer.play({url:WidgetMedia.item.data.audioUrl});
-                }
-            };
-            WidgetMedia.previous=function(){
-                audioPlayer.previous();
-            };
-            WidgetMedia.skip=function(num){
-                audioPlayer.skip(num);
-            };
-            WidgetMedia.pause=function(){
-                audioPlayer.pause();
-            };
-            WidgetMedia.next=function(){
-                audioPlayer.next();
-            };
 
-            var slider = $('#slider');
 
-            slider.onchange = function(){
-                if (Math.abs( this.value - $scope.currentTime) > 1 )
-                    audioPlayer.setTime(this.value);
-            };
-            slider.onmousedown = function(){
-                this.stopUpdateing = true;
-            };
-            slider.onmouseup = function(){
-                this.stopUpdateing = false;
+            WidgetMedia.API = null;
+
+            WidgetMedia.onPlayerReady = function ($API) {
+                WidgetMedia.API = $API;
+                console.log(WidgetMedia.API);
             };
 
             WidgetMedia.config = {
-                autoHide:false,
+                autoHide: false,
                 preload: "none",
-                sources: [
-                    {
-                        src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"),
-                        type: "video/mp4"
-                    },
-                    {
-                        src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"),
-                        type: "video/webm"
-                    },
-                    {
-                        src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"),
-                        type: "video/ogg"
-                    }
-                ],
-                tracks: [
-                    {
-                        src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-                        kind: "subtitles",
-                        srclang: "en",
-                        label: "English",
-                        default: ""
-                    }
-                ],
+                sources: undefined,
+                tracks: undefined,
                 theme: {
                     url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
                 }
             };
             WidgetMedia.changeVideoSrc = function () {
-                if(WidgetMedia.item.data.videoUrl)
+                if (WidgetMedia.item.data.videoUrl)
                     WidgetMedia.config.sources = [{
                         src: $sce.trustAsResourceUrl(WidgetMedia.item.data.videoUrl),
-                        type:  'video/' + WidgetMedia.item.data.videoUrl.split('.').pop() //"video/mp4"
+                        type: 'video/' + WidgetMedia.item.data.videoUrl.split('.').pop() //"video/mp4"
                     }];
             };
 
@@ -96,10 +36,17 @@
                 data: {}
             };
 
+            WidgetMedia.sourceChanged = function ($source) {
+                WidgetMedia.API.stop();
+                console.log(WidgetMedia.API.mediaElement[0].videoHeight);
+            };
+
+
             if (media) {
                 WidgetMedia.item = media;
                 WidgetMedia.changeVideoSrc();
             }
+
 
             AppConfig.changeBackgroundTheme(WidgetMedia.media.data.design.backgroundImage);
             var currentItemLayout = WidgetMedia.media.data.design.itemLayout;
