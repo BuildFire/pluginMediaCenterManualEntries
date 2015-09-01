c58a035ea12a557403004cf8c737fac5093c64f2
vineetasharma <vineetasharma18791@gmail.com>
2015-09-01 17:49:31 +0530

    BF-034: play and pause button added

--- widget/controllers/widget.nowplaying.controller.js
+++ widget/controllers/widget.nowplaying.controller.js
@@ -3,24 +3,32 @@
         .module('mediaCenterWidget')
         .controller('NowPlayingCtrl', ['$scope', '$routeParams', 'media', 'Buildfire', function ($scope, $routeParams, media, Buildfire) {
             var NowPlaying = this;
-                NowPlaying.track = media.data.audioUrl;
+            NowPlaying.item = media;
+            NowPlaying.playing=false;
+            NowPlaying.paused=false;
+            NowPlaying.track = media.data.audioUrl;
             var audioPlayer = Buildfire.services.media.audioPlayer;
             audioPlayer.onEvent(function (e) {
-                console.log(e);
                 if (e.event == "timeUpdate") {
                     $scope.currentTime = e.data.currentTime;
                     $scope.duration = e.data.duration;
                     $scope.$apply();
                 }
-                else if (e.event == "audioEnded" && $scope.currentTrack) {
-                    $scope.currentTrack.playing = false;
+                else if (e.event == "audioEnded") {
+                    NowPlaying.playing=false;
+                    $scope.$apply();
+                }
+                else if(e.event == "pause"){
+                    NowPlaying.playing=false;
                     $scope.$apply();
                 }
             });
             NowPlaying.playAudio = function () {
-                console.log('play called');
-                if (NowPlaying.track) {
-                    console.log(NowPlaying.track);
+                NowPlaying.playing=true;
+                if (NowPlaying.paused) {
+                    audioPlayer.play();
+                }
+                else if(NowPlaying.track) {
                     audioPlayer.play({url: NowPlaying.track});
                 }
             };
@@ -31,6 +39,7 @@
                 audioPlayer.skip(num);
             };
             NowPlaying.pause = function () {
+                NowPlaying.paused=true;
                 audioPlayer.pause();
             };
             NowPlaying.next = function () {
