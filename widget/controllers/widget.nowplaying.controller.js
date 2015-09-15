57df279320137917efc26b0f55831f66e4ecbfe0
vineetasharma <vineetasharma18791@gmail.com>
2015-09-15 17:29:12 +0530

    #1: issue fixed and removed bower dependencies

--- widget/controllers/widget.nowplaying.controller.js
+++ widget/controllers/widget.nowplaying.controller.js
@@ -1,46 +1,99 @@
 (function (angular, window) {
     angular
         .module('mediaCenterWidget')
-        .controller('NowPlayingCtrl', ['$scope', '$routeParams', 'media', 'Buildfire', 'Modals', function ($scope, $routeParams, media, Buildfire, Modals) {
+        .controller('NowPlayingCtrl', ['$scope', '$routeParams', 'media', 'Buildfire', 'Modals', 'COLLECTIONS', function ($scope, $routeParams, media, Buildfire, Modals, COLLECTIONS) {
             var NowPlaying = this;
             NowPlaying.item = media;
             NowPlaying.playing = false;
             NowPlaying.paused = false;
-            NowPlaying.track = media.data.audioUrl;
+            NowPlaying.showVolume=false;
 
+            NowPlaying.track = media.data.audioUrl;
+            /**
+             * slider to show the slider on now-playing page.
+             * @type {*|jQuery|HTMLElement}
+             */
+            NowPlaying.changeTime = function (time) {
+                audioPlayer.setTime(time);
+            };
             NowPlaying.openPlaylistPopup = function () {
-                Modals.playlistModal(NowPlaying.item).then(function(result){
-                    console.log('Result----',result);
-                },function(err){
-                    console.error('Error===========',err);
+                Modals.playlistModal(NowPlaying.item).then(function (result) {
+                    console.log('Result----', result);
+                }, function (err) {
+                    console.error('Error===========', err);
                 });
             };
             NowPlaying.openSettingsPopup = function () {
-                Modals.audioSettingsModal(NowPlaying.item).then(function(result){
-                    console.log('Result----',result);
-                },function(err){
-                    console.error('Error===========',err);
+                Modals.audioSettingsModal(NowPlaying.item).then(function (result) {
+                    console.log('Result----', result);
+                }, function (err) {
+                    console.error('Error===========', err);
                 });
             };
             NowPlaying.openMoreInfoPopup = function () {
-                Modals.moreInfoModal(NowPlaying.item).then(function(result){
-                    console.log('Result----',result);
-                },function(err){
-                    console.error('Error===========',err);
+                Modals.moreInfoModal(NowPlaying.item).then(function (result) {
+                    console.log('Result----', result);
+                }, function (err) {
+                    console.error('Error===========', err);
                 });
             };
             /**
              * audioPlayer is Buildfire.services.media.audioPlayer.
              */
             var audioPlayer = Buildfire.services.media.audioPlayer;
+            audioPlayer.settings.get(function (err,setting) {
+                NowPlaying.volume=setting.volume;
+                console.log(setting)
+            });
+
+            NowPlaying.changeVolume = function (volume) {
+                audioPlayer.settings.get(function (err,setting) {
+                    console.log(setting);
+                    if (setting) {
+                        setting.volume = volume;
+                        audioPlayer.settings.set(setting);
+                    }
+                    else {
+                        audioPlayer.settings.set({volume: volume});
+                    }
+                });
+
+            };
+            NowPlaying.shuffle = function () {
+                audioPlayer.getPlaylist(function(err,setting){
+                    console.log('err--------',err,setting);
+                });
+                audioPlayer.settings.get(function (err,setting) {
+                    if (setting) {
+                        setting.shufflePlaylist = true;
+                        audioPlayer.settings.set(setting);
+                    }
+                    else {
+                        audioPlayer.settings.set({shufflePlaylist: true});
+                    }
+                });
+
+            };
+            NowPlaying.repeatTrack = function () {
+                audioPlayer.settings.get(function (setting) {
+                    if (setting) {
+                        setting.loopPlaylist = true;
+                        audioPlayer.settings.set(setting);
+                    }
+                    else {
+                        audioPlayer.settings.set({loopPlaylist: true});
+                    }
+                });
+
+            };
             /**
              * audioPlayer.onEvent callback calls when audioPlayer event fires.
              */
             audioPlayer.onEvent(function (e) {
                 console.log(e);
                 if (e.event == "timeUpdate") {
-                    $scope.currentTime = e.data.currentTime;
-                    $scope.duration = e.data.duration;
+                    NowPlaying.currentTime = e.data.currentTime;
+                    NowPlaying.duration = e.data.duration;
                     $scope.$apply();
                 }
                 else if (e.event == "audioEnded") {
@@ -68,35 +121,34 @@
                 NowPlaying.paused = true;
                 audioPlayer.pause();
             };
-
-            NowPlaying.skip = function (num) {
-                audioPlayer.skip(num);
-            };
-
             NowPlaying.forward = function () {
-                console.log('Method called');
-                audioPlayer.skip(20);
+                if (NowPlaying.currentTime + 5 >= NowPlaying.duration)
+                    audioPlayer.setTime(NowPlaying.duration);
+                else
+                    audioPlayer.setTime(NowPlaying.currentTime + 5);
             };
 
             NowPlaying.backward = function () {
-                audioPlayer.setTime(10);
+                if (NowPlaying.currentTime - 5 > 0)
+                    audioPlayer.setTime(NowPlaying.currentTime - 5);
+                else
+                    audioPlayer.setTime(0);
             };
 
-            /**
-             * slider to show the slider on now-playing page.
-             * @type {*|jQuery|HTMLElement}
-             */
-            var slider = $('#slider');
 
-            slider.onchange = function () {
-                if (Math.abs(this.value - $scope.currentTime) > 1)
-                    audioPlayer.setTime(this.value);
-            };
-            slider.onmousedown = function () {
-                this.stopUpdateing = true;
-            };
-            slider.onmouseup = function () {
-                this.stopUpdateing = false;
-            };
-        }]);
+            Buildfire.datastore.onUpdate(function (event) {
+                console.log('Events-----------', event);
+                switch (event.tag) {
+                    case COLLECTIONS.MediaContent:
+                        if (event.data) {
+
+                            NowPlaying.item = event;
+                            $scope.$digest();
+                        }
+                        break;
+                }
+            });
+        }
+        ])
+    ;
 })(window.angular, undefined);
