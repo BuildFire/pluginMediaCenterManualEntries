1f42112c8cb3ddabfd4bf10bddae36332acb1605
vineetasharma <vineetasharma18791@gmail.com>
2016-02-01 16:04:01 +0530

    widget.nowplaying.controller code refactored

--- widget/controllers/widget.nowplaying.controller.js
+++ widget/controllers/widget.nowplaying.controller.js
@@ -23,36 +23,7 @@
                 NowPlaying.changeTime = function (time) {
                     audioPlayer.setTime(time);
                 };
-                /*NowPlaying.openPlaylistPopup = function () {
-                 Modals.playlistModal(NowPlaying.item).then(function (result) {
-                 console.log('Result----', result);
-                 }, function (err) {
-                 console.error('Error===========', err);
-                 });
-                 };
-                 NowPlaying.openSettingsPopup = function () {
-                 Modals.audioSettingsModal(NowPlaying.item).then(function (result) {
-                 console.log('Result----', result);
-                 }, function (err) {
-                 console.error('Error===========', err);
-                 });
-                 };
-                 NowPlaying.openMoreInfoPopup = function () {
-                 NowPlaying.showMoreInfo=true;
-                 /!* Modals.moreInfoModal(NowPlaying.item).then(function (result) {
-                 console.log('Result----', result);
-                 }, function (err) {
-                 console.error('Error===========', err);
-                 });*!/
-                 };
-                 NowPlaying.hideMoreInfo = function () {
-                 NowPlaying.showMoreInfo=false;
-                 /!* Modals.moreInfoModal(NowPlaying.item).then(function (result) {
-                 console.log('Result----', result);
-                 }, function (err) {
-                 console.error('Error===========', err);
-                 });*!/
-                 };*/
+
                 /**
                  * audioPlayer is Buildfire.services.media.audioPlayer.
                  */
@@ -76,52 +47,6 @@
                     });
 
                 };
-                NowPlaying.shuffle = function () {
-                    audioPlayer.getPlaylist(function (err, setting) {
-                        console.log('err--------', err, setting);
-                    });
-                    audioPlayer.settings.get(function (err, setting) {
-                        if (setting) {
-                            setting.shufflePlaylist = true;
-                            audioPlayer.settings.set(setting);
-                        }
-                        else {
-                            audioPlayer.settings.set({shufflePlaylist: true});
-                        }
-                    });
-
-                };
-                NowPlaying.repeatTrack = function () {
-                    audioPlayer.settings.get(function (setting) {
-                        if (setting) {
-                            setting.loopPlaylist = true;
-                            audioPlayer.settings.set(setting);
-                        }
-                        else {
-                            audioPlayer.settings.set({loopPlaylist: true});
-                        }
-                    });
-
-                };
-                /* /!**
-                 * audioPlayer.onEvent callback calls when audioPlayer event fires.
-                 *!/
-                 audioPlayer.onEvent(function (e) {
-                 console.log(e);
-                 if (e.event == "timeUpdate") {
-                 NowPlaying.currentTime = e.data.currentTime;
-                 NowPlaying.duration = e.data.duration;
-                 $scope.$apply();
-                 }
-                 else if (e.event == "audioEnded") {
-                 NowPlaying.playing = false;
-                 $scope.$apply();
-                 }
-                 else if (e.event == "pause") {
-                 NowPlaying.playing = false;
-                 $scope.$apply();
-                 }
-                 });*/
 
                 /**
                  * audioPlayer.onEvent callback calls when audioPlayer event fires.
@@ -342,63 +267,6 @@
                     this.shufflePlaylist = settings.shufflePlaylist;// shuffle the playlist
                 }
 
-                /*
-
-                 /!**
-                 * NowPlaying.playAudio() plays audioPlayer service.
-                 *!/
-                 NowPlaying.playAudio = function () {
-                 NowPlaying.playing = true;
-                 if (NowPlaying.paused) {
-                 audioPlayer.play();
-                 }
-                 else if (NowPlaying.track) {
-                 audioPlayer.play({url: NowPlaying.track});
-                 }
-                 };
-                 NowPlaying.pause = function () {
-                 NowPlaying.playing = false;
-                 NowPlaying.paused = true;
-                 audioPlayer.pause();
-                 };
-                 NowPlaying.forward = function () {
-                 if (NowPlaying.currentTime + 5 >= NowPlaying.duration)
-                 audioPlayer.setTime(NowPlaying.duration);
-                 else
-                 audioPlayer.setTime(NowPlaying.currentTime + 5);
-                 };
-
-                 NowPlaying.backward = function () {
-                 if (NowPlaying.currentTime - 5 > 0)
-                 audioPlayer.setTime(NowPlaying.currentTime - 5);
-                 else
-                 audioPlayer.setTime(0);
-                 };
-
-                 NowPlaying.remove = function () {
-                 console.log('remove method called');
-                 };
-
-                 NowPlaying.add = function (title, url, img, artist) {
-                 console.log('added-----------called');
-                 Modals.confirmationModal().then(function (data) {
-                 var track = new Track(title, url, img, artist);
-                 audioPlayer.addToPlaylist(track);
-                 }, function (err) {
-                 // Do somrthing on cancel
-                 });
-
-                 };
-
-                 function Track(title, url, image, artist) {
-                 this.title = title;
-                 this.url = url;
-                 this.image = image;
-                 this.artist = artist;
-                 this.startAt = 0; // where to begin playing
-                 this.lastPosition = 0; // last played to
-                 }
-                 */
 
                 Buildfire.datastore.onUpdate(function (event) {
                     console.log('Events-----------', event);
