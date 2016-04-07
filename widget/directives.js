ad19113c90eb27fa6ef0ba85ed2be59c4f1a83d5
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-04-07 22:30:56 +0530

    Replaced old placeholder images with new one

--- widget/directives.js
+++ widget/directives.js
@@ -18,5 +18,20 @@
                     $rootScope.$broadcast("Carousel:LOADED");
                 }
             };
+        }])
+        .directive("loadImage", [function () {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
+
+                    var elem = $("<img>");
+                    elem[0].onload = function () {
+                        element.attr("src", attrs.finalSrc);
+                        elem.remove();
+                    };
+                    elem.attr("src", attrs.finalSrc);
+                }
+            };
         }]);
 })(window.angular, undefined);
