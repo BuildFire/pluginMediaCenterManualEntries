5a280151a262e930bae8a91fa136c3a8638359a6
alaa smadi <alaasmadi1010@gmail.com>
2024-09-11 00:42:21 +0300

    feat(CP): refactor settings tab
    refactor settings UI, and disable inputs instead of hiding them

--- control/settings/controllers/settings.home.controller.js
+++ control/settings/controllers/settings.home.controller.js
@@ -1,249 +1,145 @@
+/* eslint-disable linebreak-style */
 (function (angular, window) {
-    'use strict';
-    angular
-        .module('mediaCenterDesign')
-        .controller('SettingsCtrl', ['$scope', 'COLLECTIONS', 'PLAYLISTINSTANCES', 'DB', function ($scope, COLLECTIONS, PLAYLISTINSTANCES, DB) {
-            var Settings = this;
-            Settings.data = {};
-            $scope.inputs = {};
-            var MediaCenter = new DB(COLLECTIONS.MediaCenter);
+	'use strict';
+	angular
+		.module('mediaCenterDesign')
+		.controller('SettingsCtrl', ['$scope', 'COLLECTIONS', 'PLAYLISTINSTANCES', 'DB', 'Messaging', function ($scope, COLLECTIONS, PLAYLISTINSTANCES, DB, Messaging) {
+			var Settings = this;
+			Settings.data = {};
+			$scope.inputs = {};
+			Settings.autoPlayDelayOptions = [
+				{ label: 'Off', value: 0 },
+				{ label: '1s', value: 1 },
+				{ label: '2s', value: 2 },
+				{ label: '3s', value: 3 },
+				{ label: '5s', value: 5 },
+			];
+			var MediaCenter = new DB(COLLECTIONS.MediaCenter);
 
-            MediaCenter.get().then((getData) => {
-                Settings.data = getData.data;
-                if (typeof (Settings.data.content.allowShare) == 'undefined')
-                    Settings.data.content.allowShare = true;
-                if (typeof (Settings.data.content.allowAddingNotes) == 'undefined')
-                    Settings.data.content.allowAddingNotes = true;
-                if (typeof (Settings.data.content.allowSource) == 'undefined')
-                    Settings.data.content.allowSource = true;
-                if (typeof (Settings.data.content.forceAutoPlay) == 'undefined')
-                    Settings.data.content.forceAutoPlay = false;
-                if (typeof (Settings.data.design.skipMediaPage) == 'undefined')
-                    Settings.data.design.skipMediaPage = false;
-                if (typeof (Settings.data.content.autoPlay) == 'undefined')
-                    Settings.data.content.autoPlay = false;
-                if (typeof (Settings.data.content.autoPlayDelay) == 'undefined')
-                    Settings.data.content.autoPlayDelay = { label: "Off", value: 0 };
-                if (typeof (Settings.data.content.globalPlaylist) == 'undefined')
-                    Settings.data.content.globalPlaylist = false;
-                if (typeof (Settings.data.content.showGlobalPlaylistNavButton) == 'undefined')
-                    Settings.data.content.showGlobalPlaylistNavButton = false;
-                if (typeof (Settings.data.content.showGlobalAddAllToPlaylistButton) == 'undefined')
-                    Settings.data.content.showGlobalAddAllToPlaylistButton = false;
-                if (typeof (Settings.data.content.globalPlaylistPlugin) == 'undefined') {
-                    Settings.data.content.globalPlaylistPlugin = false;
-                }
-                if (typeof (Settings.data.content.allowOfflineDownload) == 'undefined') {
-                    Settings.data.content.allowOfflineDownload = false;
-                }
-                if (typeof (Settings.data.content.enableFiltering) == 'undefined') {
-                    Settings.data.content.enableFiltering = false;
-                }
-                if (typeof (Settings.data.content.showViewCount) == 'undefined') {
-                    Settings.data.content.showViewCount = false;
-                }
-                if (typeof (Settings.data.content.indicatePlayedItems ) === 'undefined') {
-                    Settings.data.content.indicatePlayedItems  = false;
-                }
-                if (typeof (Settings.data.content.startWithAutoJumpByDefault  ) === 'undefined') {
-                    Settings.data.content.startWithAutoJumpByDefault   = false;
-                }
-            }, (err) => {
-                console.error(err);
-            });
+			MediaCenter.get().then((getData) => {
+				Settings.data = getData.data;
+				if (typeof (Settings.data.content.allowShare) == 'undefined')
+					Settings.data.content.allowShare = true;
+				if (typeof (Settings.data.content.allowAddingNotes) == 'undefined')
+					Settings.data.content.allowAddingNotes = true;
+				if (typeof (Settings.data.content.allowSource) == 'undefined')
+					Settings.data.content.allowSource = true;
+				if (typeof (Settings.data.content.forceAutoPlay) == 'undefined')
+					Settings.data.content.forceAutoPlay = false;
+				if (typeof (Settings.data.design.skipMediaPage) == 'undefined')
+					Settings.data.design.skipMediaPage = false;
+				if (typeof (Settings.data.content.autoPlay) == 'undefined')
+					Settings.data.content.autoPlay = false;
+				if (typeof (Settings.data.content.autoPlayDelay) == 'undefined')
+					Settings.data.content.autoPlayDelay = { label: 'Off', value: 0 };
+				if (typeof (Settings.data.content.globalPlaylist) == 'undefined')
+					Settings.data.content.globalPlaylist = false;
+				if (typeof (Settings.data.content.showGlobalPlaylistNavButton) == 'undefined')
+					Settings.data.content.showGlobalPlaylistNavButton = false;
+				if (typeof (Settings.data.content.showGlobalAddAllToPlaylistButton) == 'undefined')
+					Settings.data.content.showGlobalAddAllToPlaylistButton = false;
+				if (typeof (Settings.data.content.globalPlaylistPlugin) == 'undefined') {
+					Settings.data.content.globalPlaylistPlugin = false;
+				}
+				if (typeof (Settings.data.content.allowOfflineDownload) == 'undefined') {
+					Settings.data.content.allowOfflineDownload = false;
+				}
+				if (typeof (Settings.data.content.enableFiltering) == 'undefined') {
+					Settings.data.content.enableFiltering = false;
+				}
+				if (typeof (Settings.data.content.showViewCount) == 'undefined') {
+					Settings.data.content.showViewCount = false;
+				}
+				if (typeof (Settings.data.content.indicatePlayedItems ) === 'undefined') {
+					Settings.data.content.indicatePlayedItems  = false;
+				}
+				if (typeof (Settings.data.content.startWithAutoJumpByDefault  ) === 'undefined') {
+					Settings.data.content.startWithAutoJumpByDefault   = false;
+				}
 
-            Settings.setSettings = () => {
-                MediaCenter.save(Settings.data).then(() => { });
-            }
+				$scope.setupSettingsWatch();
+			}, (err) => {
+				console.error(err);
+			});
 
-            Settings.setForceAutoPlay = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.forceAutoPlay) {
-                    if (value === true && Settings.data.content.autoPlay) {
-                        Settings.data.content.autoPlay = false;
-                    }
-                    Settings.data.content.forceAutoPlay = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            }
+			Settings.setSettings = () => {
+				MediaCenter.save(Settings.data).then(() => {
+					Messaging.sendMessageToWidget({ cmd: 'settings', data: Settings.data });
+				}).catch((err) => {
+					// TODO: handle error
+					console.error(err);
+				});
+			};
 
-            Settings.changeSkipPage = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.design.skipMediaPage) {
-                    if (value === false) {
-                        Settings.data.content.autoPlay = false;
-                    }
-                    Settings.data.design.skipMediaPage = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            };
 
-            Settings.setAllowSource = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.allowSource) {
-                    Settings.data.content.allowSource = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            }
+			Settings.showPluginsDialog = () => {
+				// MCM Playlist plugin unique Id
+				buildfire.pluginInstance.showDialog({}, (err, instances) => {
+					if (err) {
+						return buildfire.dialog.toast({
+							message: 'Error occured',
+							type: 'danger',
+						});
+					}
+					if (instances && instances.length > 0) {
+						if (instances[0].pluginTypeId === PLAYLISTINSTANCES.DEV || instances[0].pluginTypeId === PLAYLISTINSTANCES.QA || instances[0].pluginTypeId === PLAYLISTINSTANCES.PROD) {
+							Settings.data.content.globalPlaylistPlugin = instances[0];
+						} else {
+							buildfire.dialog.toast({
+								message: 'Please select the correct paylist plugin',
+								type: 'warning',
+							});
+						}
+					}
+				});
+			};
 
-            Settings.setAllowShare = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.allowShare) {
-                    Settings.data.content.allowShare = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            }
+			Settings.deleteActionItem = () => {
+				Settings.data.content.globalPlaylistPlugin = null;
+			};
 
-            Settings.setAllowAddingNotes = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.allowAddingNotes) {
-                    Settings.data.content.allowAddingNotes = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            }
+			$scope.saveTimer = null;
+			$scope.formatSettingsAndSave = () => {
+				if (!Settings.data || !Settings.data.content) return;
+                
+				if (Settings.data.content.forceAutoPlay) {
+					Settings.data.content.autoPlay = false;
+				}
 
-            Settings.setAutoPlay = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.autoPlay) {
-                    if (value === true && Settings.data.content.forceAutoPlay) {
-                        Settings.data.content.forceAutoPlay = false;
-                    }
-                    if (value === true) Settings.data.design.skipMediaPage = true;
-                    Settings.data.content.autoPlay = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            };
+				if (Settings.data.content.autoPlay) {
+					Settings.data.content.skipMediaPage = true;
+				}
 
-            Settings.setAutoPlayDelay = (option) => {
-                if (option.value != Settings.data.content.autoPlayDelay.value) {
-                    Settings.data.content.autoPlayDelay = option;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            };
+				if (!Settings.data.content.globalPlaylist || !Settings.data.content.globalPlaylistPlugin || !Settings.data.content.globalPlaylistPlugin.title) {
+					Settings.data.content.showGlobalAddAllToPlaylistButton = false;
+					Settings.data.content.showGlobalPlaylistNavButton = false;
+				}
 
-            Settings.setGlobalPlaylist = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.globalPlaylist) {
-                    Settings.data.content.globalPlaylist = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            };
+				if (Settings.data.content.enableFiltering && !Settings.data.content.filterPageDeeplink) {
+					const instanceId = buildfire.getContext().instanceId;
+					Settings.data.content.filterPageDeeplink = `${instanceId}_filterScreen`;
+					buildfire.deeplink.registerDeeplink(
+						{
+							id: Settings.data.content.filterPageDeeplink,
+							name: 'Filter Screen',
+							deeplinkData: { screen: 'filterScreen' },
+						},
+						(err, result) => {
+							if (err) return console.log(err);
+						}
+					);
+				}
 
-            Settings.setGlobalPlaylistNavButton = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.showGlobalPlaylistNavButton) {
-                    Settings.data.content.showGlobalPlaylistNavButton = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            };
+				if ($scope.saveTimer) clearTimeout($scope.saveTimer);
+				$scope.saveTimer = setTimeout(() => {
+					Settings.setSettings();
+				}, 500);
+			};
 
-            Settings.setGlobalPlaylistAddButton = (e) => {
-                let value = !e.target.checked;
-                if (value != Settings.data.content.showGlobalAddAllToPlaylistButton) {
-                    Settings.data.content.showGlobalAddAllToPlaylistButton = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            };
-
-            Settings.setAllowOfflineDownload = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.allowOfflineDownload) {
-                    Settings.data.content.allowOfflineDownload = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            };
-
-            Settings.setEnableFiltering = (e) => {
-                let value = e.target.checked;
-                if (value && !Settings.data.content.filterPageDeeplink) {
-                    console.log("creating deeplink for filter page");
-                    buildfire.getContext((err, context) => {
-                        if (err) return console.log(`ERROR:${err}`);
-                        var instanceId = context.instanceId;
-                        buildfire.deeplink.registerDeeplink(
-                            {
-                                id: `${instanceId}_filterScreen`,
-                                name: "Filter Screen",
-                                deeplinkData: { screen: "filterScreen" },
-                            },
-                            (err, result) => {
-                                if (err) return console.log(err);
-                                if (value != Settings.data.content.enableFiltering) {
-                                    Settings.data.content.enableFiltering = value;
-                                    Settings.data.content.filterPageDeeplink = `${instanceId}_filterScreen`;
-                                    console.log("filter page dl", result);
-                                    MediaCenter.save(Settings.data).then(() => { });
-                                }
-                            }
-                        );
-                    })
-                }
-                else {
-                    if (value != Settings.data.content.enableFiltering) {
-                        Settings.data.content.enableFiltering = value;
-                        MediaCenter.save(Settings.data).then(() => { });
-                    }
-                }
-
-            };
-
-            Settings.setEnablePlayCount = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.showViewCount) {
-                    Settings.data.content.showViewCount = value;
-                    MediaCenter.save(Settings.data).then(() => { });
-                }
-            }
-
-            Settings.setEnableIndicatePlayedItems = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.indicatePlayedItems ) {
-                    Settings.data.content.indicatePlayedItems  = value;
-                    MediaCenter.save(Settings.data);
-                }
-            }
-
-            Settings.setEnableStartWithAutoJumpByDefault = (e) => {
-                let value = e.target.checked;
-                if (value != Settings.data.content.startWithAutoJumpByDefault  ) {
-                    Settings.data.content.startWithAutoJumpByDefault   = value;
-                    MediaCenter.save(Settings.data);
-                }
-            }
-
-            Settings.setGlobalPlaylistPlugin = (pluginInstance) => {
-                Settings.data.content.globalPlaylistPlugin = pluginInstance;
-                MediaCenter.save(Settings.data).then(() => { });
-            };
-
-            Settings.autoPlayDelayOptions = [
-                { label: "Off", value: 0 },
-                { label: "1s", value: 1 },
-                { label: "2s", value: 2 },
-                { label: "3s", value: 3 },
-                { label: "5s", value: 5 },
-            ];
-
-            Settings.showPluginsDialog = () => {
-                // MCM Playlist plugin unique Id
-                buildfire.pluginInstance.showDialog({}, (err, instances) => {
-                    if (err) {
-                        return buildfire.dialog.toast({
-                            message: "Error occured",
-                            type: "danger",
-                        });
-                    }
-                    if (instances && instances.length > 0) {
-                        if (instances[0].pluginTypeId === PLAYLISTINSTANCES.DEV || instances[0].pluginTypeId === PLAYLISTINSTANCES.QA || instances[0].pluginTypeId === PLAYLISTINSTANCES.PROD) {
-                            Settings.setGlobalPlaylistPlugin(instances[0]);
-                        } else {
-                            buildfire.dialog.toast({
-                                message: "Please select the correct paylist plugin",
-                                type: "warning",
-                            });
-                        }
-                    }
-                });
-            }
-        }]);
+			$scope.setupSettingsWatch = () => {
+				$scope.$watch(function () {
+					return Settings.data;
+				}, $scope.formatSettingsAndSave, true);
+			};
+		}]);
 })(window.angular, window);
