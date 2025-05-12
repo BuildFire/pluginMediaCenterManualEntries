992b8209ebbbf5861c9b4476824881bb065c61b5
AliAlaqrabawi3 <167025219+AliAlaqrabawi3@users.noreply.github.com>
2025-05-12 20:08:04 +0300

    Update services.js

--- control/content/services.js
+++ control/content/services.js
@@ -208,30 +208,29 @@
                 var that = this;
                 var deferred = $q.defer();
 
-				if (typeof key === 'undefined') {
-					var error = new Error(MESSAGES.ERROR.ID_NOT_DEFINED);
-					deferred.reject(error);
-					callback(error, null);
-					return deferred.promise;
-				}
-
+                if (typeof key === 'undefined') {
+                    var error = new Error(MESSAGES.ERROR.ID_NOT_DEFINED);
+                    deferred.reject(error);
+                    callback(error, null);
+                    return deferred.promise;
+                }
                 var data = {
-                key,
-                tag: that._tagName
+                    key,
+                    tag: that._tagName
                 };
 
                 Buildfire.services.searchEngine.delete(data, function (err, result) {
                     if (err) {
                         if (err.errorMessage && err.errorMessage === 'Not Found'){
-							deferred.resolve(true);
+                            deferred.resolve(true);
                             callback(null, true);
-						}
-						else {
-							deferred.reject(err);
-							callback(err, null);
-                    }
+                        }
+                        else {
+                            deferred.reject(err);
+                            callback(err, null);
+                        }
                     } else if (result) {
-						deferred.resolve(result);
+                        deferred.resolve(result);
                         callback(null, result);
                     }
                 });
