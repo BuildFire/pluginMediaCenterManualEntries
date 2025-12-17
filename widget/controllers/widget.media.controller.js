097103c2b22e541565e8d0f26b91f7c4164de41c
alaa smadi <alaasmadi1010@gmail.com>
2025-12-17 19:44:48 +0300

    remove old drawer functionalities

--- widget/controllers/widget.media.controller.js
+++ widget/controllers/widget.media.controller.js
@@ -98,116 +98,6 @@
                     $scope.$apply();
                 }
 
-                WidgetMedia.showDrawer = function () {
-                    let listItems = [];
-                    if (WidgetMedia.media.data.content.allowAddingNotes !== false && $rootScope.online) {
-                        listItems.push({ id: "addNote", text: getString("itemDrawer.addNote") });
-                    }
-                    if (WidgetMedia.media.data.content.allowOfflineDownload) {
-                        if (WidgetMedia.item.data.videoUrl && $rootScope.online) {
-                            if (WidgetMedia.item.data.hasDownloadedVideo) {
-                                listItems.push({ id: "removeDownloadedVideo", text: getString("itemDrawer.removeDownloadedVideo") });
-                            }
-
-                            else {
-                                if ($rootScope.currentlyDownloading.indexOf(WidgetMedia.item.id) < 0)
-                                    listItems.push({ id: "downloadVideo", text: getString("itemDrawer.downloadVideo") });
-                            }
-                        }
-                        if (WidgetMedia.item.data.audioUrl) {
-                            if (WidgetMedia.item.data.hasDownloadedAudio) {
-                                listItems.push({ id: "removeDownloadedAudio", text: getString("homeDrawer.removeDownloadedAudio") });
-                            }
-                            else {
-                                if ($rootScope.currentlyDownloading.indexOf(WidgetMedia.item.id) < 0)
-                                    listItems.push({ id: "downloadAudio", text: getString("homeDrawer.downloadAudio") });
-                            }
-                        }
-                    }
-
-                    if (WidgetMedia.media.data.content.allowShare && $rootScope.online) {
-                        listItems.push({ id: "share", text: getString("itemDrawer.share") });
-                    }
-
-                    if (WidgetMedia.item.data.links.length && $rootScope.online) {
-                        listItems.push({ id: "openLinks", text: getString("itemDrawer.openLinks") });
-                    }
-
-                    if (WidgetMedia.media.data.content.globalPlaylist && $rootScope.online && (WidgetMedia.item.data.audioUrl || WidgetMedia.item.data.videoUrl)) {
-                        if ($rootScope.isInGlobalPlaylist(WidgetMedia.item.id)) {
-                            listItems.push({ id: "removeFromPlaylist", text: getString("itemDrawer.removeFromPlaylist") });
-                        }
-                        else {
-                            listItems.push({ id: "addToPlaylist", text: getString("itemDrawer.addToPlaylist") });
-                        }
-                    }
-
-                    if ($rootScope.online) {
-                        if (WidgetMedia.item.data.bookmarked) {
-                            listItems.push({ id: "removeFromFavorites", text: getString("itemDrawer.removeFromFavorites") });
-                        }
-                        else {
-                            listItems.push({ id: "favorite", text: getString("itemDrawer.favorite") });
-                        }
-                    }
-
-                    buildfire.components.drawer.open(
-                        {
-                            isHTML: false,
-                            listItems: listItems
-                        },
-                        (err, result) => {
-                            if (err) return console.error(err);
-                            buildfire.components.drawer.closeDrawer();
-                            if (result) {
-                                if (result.id == "downloadVideo") {
-                                    $rootScope.download(WidgetMedia.item, "video");
-                                }
-
-                                if (result.id == "removeDownloadedVideo") {
-                                    $rootScope.removeDownload(WidgetMedia.item, "video");
-                                }
-
-                                if (result.id == "downloadAudio") {
-                                    $rootScope.download(WidgetMedia.item, "audio");
-                                }
-
-                                if (result.id == "removeDownloadedAudio") {
-                                    $rootScope.removeDownload(WidgetMedia.item, "audio");
-                                }
-
-                                if (result.id == "share") {
-                                    WidgetMedia.share(WidgetMedia.item);
-                                }
-
-                                if (result.id == "openLinks") {
-                                    WidgetMedia.openLinks(WidgetMedia.item.data.links);
-                                }
-
-                                if (result.id == "addToPlaylist") {
-                                    $rootScope.toggleGlobalPlaylistItem(WidgetMedia.item);
-                                }
-
-                                if (result.id == "removeFromPlaylist") {
-                                    $rootScope.toggleGlobalPlaylistItem(WidgetMedia.item);
-                                }
-
-                                if (result.id == "addNote") {
-                                    WidgetMedia.addNote(WidgetMedia.item);
-                                }
-
-                                if (result.id == "removeFromFavorites") {
-                                    WidgetMedia.bookmark(WidgetMedia.item);
-                                }
-
-                                if (result.id == "favorite") {
-                                    WidgetMedia.bookmark(WidgetMedia.item);
-                                }
-                            }
-                        }
-                    );
-                };
-
                 WidgetMedia.openMediaComments = function (commentIds = []) {
                     buildfire.spinner.show();
 
@@ -384,6 +274,7 @@
 
                     if (iconId === 'share') getString("itemDrawer.share");
                     if (iconId === 'sourceLink') getString("itemDrawer.openLinks");
+                    if (iconId === 'openActionLinks') getString("itemDrawer.openLinks");
 
                     if (iconId === 'favorite') {
                         if (WidgetMedia.item.data.bookmarked) return getString("itemDrawer.removeFromFavorites");
@@ -406,6 +297,7 @@
                         {id: 'downloadVideo', iconName: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">\n<g clip-path="url(#clip0_1664_11661)">\n<path d="M3 5H21V10H23V5C23 3.9 22.1 3 21 3H3C1.9 3 1 3.9 1 5V17C1 18.1 1.9 19 3 19H12V17H3V5Z" fill="#46BFE6"/>\n<path d="M15 11L9 7V15L15 11Z" fill="#46BFE6"/>\n<path d="M24 18.4167L22.59 17.1242L20 19.4892L20 12L18 12L18 19.4892L15.41 17.1242L14 18.4167L19 23L24 18.4167Z" fill="#46BFE6"/>\n</g>\n<defs>\n<clipPath id="clip0_1664_11661">\n<rect width="24" height="24" fill="white"/>\n</clipPath>\n</defs>\n</svg>'},
                         {id: 'downloadAudio', iconName: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">\n<g clip-path="url(#clip0_1664_11652)">\n<path d="M8 3L8.01 13.55C7.41 13.21 6.73 13 6.01 13C3.79 13 2 14.79 2 17C2 19.21 3.79 21 6.01 21C8.23 21 10 19.21 10 17V7H14V3H8Z" fill="#46BFE6"/>\n<path d="M22 16L20.59 14.59L18 17.17L18 9L16 9L16 17.17L13.41 14.59L12 16L17 21L22 16Z" fill="#46BFE6"/>\n</g>\n<defs>\n<clipPath id="clip0_1664_11652">\n<rect width="24" height="24" fill="white"/>\n</clipPath>\n</defs>\n</svg>'},
                         {id: 'sourceLink', iconName: '<i class="glyphicon share glyphicon-link" role="button" aria-label="Audio Item source button"></i>'},
+                        {id: 'openActionLinks', iconName: 'flowchart'},
                         { ...getPlaylistIcon() }, // playlist icon
                     ];
 
@@ -420,6 +312,9 @@
                     }
                     if (!WidgetMedia.media.data.content.allowAddingNotes) {
                         icons = icons.filter(_icon => _icon.id !== 'note');
+                    }
+                    if (!WidgetMedia.item.data.links || !WidgetMedia.item.data.links.length || !$rootScope.online) {
+                        icons = icons.filter(_icon => _icon.id !== 'openActionLinks');
                     }
                     if (!WidgetMedia.media.data.content.allowOfflineDownload) {
                         icons = icons.filter(_icon => _icon.id !== 'downloadVideo' && _icon.id !== 'downloadAudio');
@@ -479,7 +374,6 @@
                                 iconEl.querySelector('.material-icons-outlined').classList.add('flip-horizontal')
                                 iconEl.innerHTML += '<span class="margin-left-five count-container">0</span>';
                                 getCommentsCount().then(count => {
-                                    count = 12522;
                                     iconEl.querySelector('.count-container').innerHTML = count.toLocaleString('en-US');
                                 }).catch((err) => {
                                     console.error(err);
@@ -490,7 +384,7 @@
                                     if (err) console.error(err);
 
                                     if (res && WidgetMedia) {
-                                        WidgetMedia.count = 12522;;
+                                        WidgetMedia.count = res.totalRecord;
                                         if (!$scope.$$phase) $scope.$apply();
                                         iconEl.querySelector('.count-container').innerHTML = WidgetMedia.count.toLocaleString('en-US');
                                     }
@@ -507,9 +401,17 @@
                             case 'share': WidgetMedia.share(); break;
                             case 'favorite': WidgetMedia.bookmark(); break;
                             case 'note': WidgetMedia.addNote(); break;
-                            case 'downloadVideo': $rootScope.download(WidgetMedia.item, 'video'); break;
-                            case 'downloadAudio': $rootScope.download(WidgetMedia.item, 'audio'); break;
+                            case 'downloadVideo':
+								if (WidgetMedia.item.data.hasDownloadedVideo) $rootScope.removeDownload(WidgetMedia.item, "video");
+								else $rootScope.download(WidgetMedia.item, "video");
+								break;
+                            case 'downloadAudio':
+								if (WidgetMedia.item.data.hasDownloadedAudio) $rootScope.removeDownload(WidgetMedia.item, "audio");
+								else $rootScope.download(WidgetMedia.item, "audio");
+								break;
                             case 'sourceLink': WidgetMedia.openLink(WidgetMedia.item); break;
+                            case 'openActionLinks': WidgetMedia.openLinks(WidgetMedia.item.data.links); break;
+                            case 'playlist': $rootScope.toggleGlobalPlaylistItem(WidgetMedia.item); break;
                         }
                     };
 
