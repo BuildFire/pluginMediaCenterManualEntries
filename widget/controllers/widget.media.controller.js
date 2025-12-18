7443745908e5390084533dc9016ac539b37cfdef
alaa smadi <alaasmadi1010@gmail.com>
2025-12-18 04:02:49 +0300

    refactor icons alignment

--- widget/controllers/widget.media.controller.js
+++ widget/controllers/widget.media.controller.js
@@ -311,7 +311,7 @@
 
                 WidgetMedia.initMediaActionIcons = () => {
                     let icons = [
-                        {id: 'views', iconName: 'visibility'},
+                        {id: 'views', iconName: 'visibility', filled: true},
                         {id: 'comments', iconName: 'chat_bubble_outline'},
                         {id: 'share', iconName: 'share'},
                         { ...getFavoriteIcon() }, // favorite icon
@@ -319,7 +319,7 @@
                         {id: 'downloadVideo', iconName: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">\n<g clip-path="url(#clip0_1664_11661)">\n<path d="M3 5H21V10H23V5C23 3.9 22.1 3 21 3H3C1.9 3 1 3.9 1 5V17C1 18.1 1.9 19 3 19H12V17H3V5Z" fill="#46BFE6"/>\n<path d="M15 11L9 7V15L15 11Z" fill="#46BFE6"/>\n<path d="M24 18.4167L22.59 17.1242L20 19.4892L20 12L18 12L18 19.4892L15.41 17.1242L14 18.4167L19 23L24 18.4167Z" fill="#46BFE6"/>\n</g>\n<defs>\n<clipPath id="clip0_1664_11661">\n<rect width="24" height="24" fill="white"/>\n</clipPath>\n</defs>\n</svg>'},
                         {id: 'downloadAudio', iconName: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">\n<g clip-path="url(#clip0_1664_11652)">\n<path d="M8 3L8.01 13.55C7.41 13.21 6.73 13 6.01 13C3.79 13 2 14.79 2 17C2 19.21 3.79 21 6.01 21C8.23 21 10 19.21 10 17V7H14V3H8Z" fill="#46BFE6"/>\n<path d="M22 16L20.59 14.59L18 17.17L18 9L16 9L16 17.17L13.41 14.59L12 16L17 21L22 16Z" fill="#46BFE6"/>\n</g>\n<defs>\n<clipPath id="clip0_1664_11652">\n<rect width="24" height="24" fill="white"/>\n</clipPath>\n</defs>\n</svg>'},
                         {id: 'sourceLink', iconName: '<i class="glyphicon share glyphicon-link" role="button" aria-label="Audio Item source button"></i>'},
-                        {id: 'openActionLinks', iconName: 'device_hub'},
+                        {id: 'openActionLinks', iconName: 'open_in_new'},
                         { ...getPlaylistIcon() }, // playlist icon
                     ];
 
@@ -354,6 +354,7 @@
                     const mediaActionIconsContainer = document.getElementById('mediaActionIcons');
                     if (!mediaActionIconsContainer) return;
                     mediaActionIconsContainer.innerHTML = '';
+                    mediaActionIconsContainer.classList.remove('justify-content-start');
 
                     let maxIconsCount = 5;
                     if (WidgetMedia.media.data && WidgetMedia.media.data.design && WidgetMedia.media.data.design.itemLayout === "item-2") {
@@ -363,8 +364,12 @@
                     const visibleIcons = icons.slice(0, maxIconsCount);
                     const hiddenIcons = icons.slice(maxIconsCount);
 
-                    visibleIcons.forEach((icon, index) => {
-                        if (index === maxIconsCount-1 && hiddenIcons.length > 0) {
+                    for (let index = 0; index < maxIconsCount; index++) {
+                        const icon = visibleIcons[index];
+
+                        if (!icon) {
+                            mediaActionIconsContainer.classList.add('justify-content-start');
+                        } else if (index === maxIconsCount-1 && hiddenIcons.length > 0) {
                             const moreIcon = document.createElement('div');
                             moreIcon.classList.add('flex');
                             moreIcon.classList.add('flex-align-center');
@@ -390,7 +395,7 @@
                                 if (index === (maxIconsCount - 1)) newClass = 'flex-justify-end';
                                 iconEl.classList.add(newClass);
                             }
-                            iconEl.innerHTML = icon.iconName.includes('<') ? icon.iconName : `<i class="material-icons-outlined">${icon.iconName}</i>`;
+                            iconEl.innerHTML = icon.iconName.includes('<') ? icon.iconName : `<i class="${icon.filled ? 'material-icons' : 'material-icons-outlined'}">${icon.iconName}</i>`;
                             iconEl.onclick = () => handleIconAction(icon.id);
                             if (icon.id === 'comments') {
                                 iconEl.querySelector('.material-icons-outlined').classList.add('flip-horizontal')
@@ -414,7 +419,7 @@
                             }
                             mediaActionIconsContainer.appendChild(iconEl);
                         }
-                    });
+                    }
 
                     const handleIconAction = (actionId) => {
                         switch(actionId) {
@@ -916,6 +921,13 @@
                     if (!params.commentId) {
                         WidgetMedia.playAudio();
                     }
+                }
+
+                WidgetMedia.resizeImage = (src) => {
+                    const resizedImage = buildfire.imageLib.resizeImage(src,
+                        { size: "half_width", aspect: "16:9" }
+                    );
+                    return resizedImage;
                 }
 
                 var initializing = true;
